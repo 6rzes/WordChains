31c8e8e6ed935f0b39055b20abf396ef4b2a0e7a
GUllmann <gullmann@gmail.com>
2018-07-25 15:49:16 +0200

    modification of existing tests, added new tests

--- src/test/java/app/controller/WordChainsTest.java
+++ src/test/java/app/controller/WordChainsTest.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 public class WordChainsTest {
-    
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -28,18 +28,64 @@ public class WordChainsTest {
                 .perform(get("/?initialWord=cat&lastWord=dog"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("[\"cat\",\"cot\",\"cog\",\"dog\"]")));
+                .andExpect(content().string(containsString("[cat, cot, cog, dog]")));
 
         mockMvc
                 .perform(get("/?initialWord=lead&lastWord=gold"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("[\"lead\",\"load\",\"goad\",\"gold\"]")));
+                .andExpect(content().string(containsString("[lead, load, goad, gold]")));
 
         mockMvc
                 .perform(get("/?initialWord=ruby&lastWord=code"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("[\"ruby\",\"rube\",\"robe\",\"rode\",\"code\"]")));
+                .andExpect(content().string(containsString("[ruby, rube, robe, rode, code]")));
+
+        mockMvc
+                .perform(get("/?initialWord=&lastWord=code"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Invalid data! initialWord:  lastWord: code")));
+
+        mockMvc
+                .perform(get("/?initialWord=&lastWord="))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Invalid data! initialWord:  lastWord: ")));
+
+        mockMvc
+                .perform(get("/?initialWord=www&lastWord=zzzzzzzz"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Invalid data! Words length should be equal. initialWord: www lastWord: zzzzzzzz")));
+
+        mockMvc
+                .perform(get("/?initialWord=cot&lastWord=lead"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Invalid data! Words length should be equal. initialWord: cot lastWord: lead")));
+
+        mockMvc
+                .perform(get("/?initialWord=xyzxyz&lastWord=abcabc"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Path not found. initialWord: xyzxyz lastWord: abcabc")));
+
+        mockMvc
+                .perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+
+        mockMvc
+                .perform(get("/?lastWord=abcabc"))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+
+        mockMvc
+                .perform(get("/?initialWord="))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+
     }
 }

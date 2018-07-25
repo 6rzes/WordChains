8c4dcad0b343d5f0956784c4d9663526b7b70f0d
GUllmann <gullmann@gmail.com>
2018-07-25 15:48:49 +0200

    Update controller, add properties, add tests

--- src/main/java/app/controller/ControllerWordChains.java
+++ src/main/java/app/controller/ControllerWordChains.java
@@ -3,6 +3,8 @@ package app.controller;
 import app.service.ServiceDictionary;
 import app.service.ServiceWordChains;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,25 +12,47 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 @Controller
+@PropertySource("classpath:config.properties")
 public class ControllerWordChains {
 
+    @Autowired
+    Environment env;
     @Autowired
     private ServiceDictionary serviceDictionary;
     @Autowired
     private ServiceWordChains serviceWordChains;
 
-    private static final String LINK_TO_REMOTE_FILE = "http://codekata.com/data/wordlist.txt";
-    private static final String LOCAL_FILE_NAME = "src\\main\\resources\\wordlist.txt";
-
     @GetMapping(value = "/")
     @ResponseStatus(HttpStatus.OK)
-    public @ResponseBody List<String> getWordPath(
-            @RequestParam(value = "initialWord", required = true) String initialWord,
-            @RequestParam(value = "lastWord",required = true) String lastWord) {
-        List <String> wordList = serviceDictionary.getReducedWordList(LINK_TO_REMOTE_FILE, LOCAL_FILE_NAME, initialWord.length());
-        return serviceWordChains.getPathWay(wordList,initialWord,lastWord);
+    public @ResponseBody
+    String getWordPath(
+            @RequestParam(value = "initialWord") String initialWord,
+            @RequestParam(value = "lastWord") String lastWord) {
+
+        if (initialWord.equals("") || lastWord.equals("")) {
+            return MessageFormat.format(env.getProperty("invalidDataNoWord"), initialWord, lastWord);
+        }
+
+        if (initialWord.length() != lastWord.length()) {
+            return MessageFormat.format(env.getProperty("invalidDataWordsLength"), initialWord, lastWord);
+        }
+
+        List<String> wordList = serviceDictionary.getReducedWordList(
+                env.getProperty("remoteFileLink"),env.getProperty("localFileName"), initialWord.length());
+
+        if (wordList == null || wordList.isEmpty()) {
+            return MessageFormat.format(env.getProperty("invalidDataWordNotFound"), initialWord, lastWord);
+        }
+
+        List<String> pathWay = serviceWordChains.getPathWay(wordList, initialWord, lastWord);
+        if(pathWay == null || pathWay.isEmpty()) {
+            return MessageFormat.format(env.getProperty("invalidDataPathNotFound"), initialWord, lastWord);
+        }
+
+        return serviceWordChains.getPathWay(wordList, initialWord, lastWord).toString();
     }
 }
